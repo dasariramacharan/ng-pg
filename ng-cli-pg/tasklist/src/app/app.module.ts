a6cabb50459e4ce44dc6ba39dae06336f37f5475
Rama Dasari <12179940+dasariramacharan@users.noreply.github.com>
2018-06-04 23:30:04 +0800

    ng-cli-pg: Added tasks service and details component

--- ng-cli-pg/tasklist/src/app/app.module.ts
+++ ng-cli-pg/tasklist/src/app/app.module.ts
@@ -4,17 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AllTasksComponent } from './all-tasks/all-tasks.component';
+import { TaskDetailsComponent } from './task-details/task-details.component';
+
+import { TaskService } from './core/task.service';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AllTasksComponent
+    AllTasksComponent,
+    TaskDetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [TaskService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
