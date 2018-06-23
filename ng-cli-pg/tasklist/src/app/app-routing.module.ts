f5b69ff2ec264b88621633ba64d7d13daf3b9473
Rama Dasari <12179940+dasariramacharan@users.noreply.github.com>
2018-06-23 15:55:29 +0800

    fixed werid issue of child routes not detected. Resolved by restarting visual studio code/ ng serve i suppose. this happened 2nd time

--- ng-cli-pg/tasklist/src/app/app-routing.module.ts
+++ ng-cli-pg/tasklist/src/app/app-routing.module.ts
@@ -13,8 +13,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)
-  ],
+  imports: [RouterModule.forRoot(routes,
+    { enableTracing: true } // <-- debugging purposes only)
+  )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
