3e046e106c6b79e45f8f6fe926e70dd224df262f
Rama Dasari <12179940+dasariramacharan@users.noreply.github.com>
2018-07-02 23:46:01 +0800

    modified dashboard to show last 5 items to view the recently added items

--- ng-cli-pg/tasklist/src/app/people/people-dashboard/people-dashboard.component.ts
+++ ng-cli-pg/tasklist/src/app/people/people-dashboard/people-dashboard.component.ts
@@ -22,7 +22,7 @@ export class PeopleDashboardComponent implements OnInit {
   getPeople() :void{
 
     this.peopleService.getPeople()
-                      .subscribe(people => this.people = people.splice(1,5));
+                      .subscribe(people => this.people = people.splice(people.length - 5,5));
   }
   
   onPersonSelected(personid){
@@ -40,10 +40,4 @@ export class PeopleDashboardComponent implements OnInit {
     console.log(person);
     console.log(this.people)
   }
-
-
-
-
-  
-
 }
