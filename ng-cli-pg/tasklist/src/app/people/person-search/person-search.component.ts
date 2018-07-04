46e41b551e76a2394e676bef39ee365729d5b1d8
Rama Dasari <12179940+dasariramacharan@users.noreply.github.com>
2018-07-04 22:09:35 +0800

    fixed exception while doing search

--- ng-cli-pg/tasklist/src/app/people/person-search/person-search.component.ts
+++ ng-cli-pg/tasklist/src/app/people/person-search/person-search.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
+import { Person } from '../person';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { PeopleService } from 'src/app/people/shared/people.service';
 
 @Component({
   selector: 'rd-person-search',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonSearchComponent implements OnInit {
 
-  constructor() { }
+  //A Subject is both a source of observable values and an Observable itself. 
+  //You can subscribe to a Subject as you would any Observable
+  private searchTerms = new Subject<string>();
+  searchResults$: Observable<Person[]>;
+
+  constructor(private peopleService:PeopleService) { }
+   
+   //push a search term into the observable stream
+   search(term: string) :void {
+     this.searchTerms.next(term);
+   }
 
   ngOnInit() {
+    this.searchResults$ = this.searchTerms.pipe(
+
+      // wait 300ms after each keystroke before considering the term
+      debounceTime(300),
+ 
+      // ignore new term if same as previous term
+      distinctUntilChanged(),
+ 
+      // switch to new search observable each time the term changes
+      switchMap((term: string) => this.peopleService.searchPersons(term)),
+    );
+
   }
+
+  
+
 
 }
