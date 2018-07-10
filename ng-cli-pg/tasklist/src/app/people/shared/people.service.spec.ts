f46445a30d0b7cc1f879f50f335b785aedaf8bdd
Rama Dasari <12179940+dasariramacharan@users.noreply.github.com>
2018-07-10 23:19:51 +0800

    added unit test for observable

--- ng-cli-pg/tasklist/src/app/people/shared/people.service.spec.ts
+++ ng-cli-pg/tasklist/src/app/people/shared/people.service.spec.ts
@@ -12,4 +12,15 @@ describe('PeopleService', () => {
   it('should be created', inject([PeopleService], (service: PeopleService) => {
     expect(service).toBeTruthy();
   }));
+  
+  it('#getPeople should return value from observable',
+  inject([PeopleService], 
+    (service: PeopleService) => {
+    service.getPeople().subscribe(value => {
+      expect(value).not.toBe(null);
+      });
+  })
+);
+
+
 });
